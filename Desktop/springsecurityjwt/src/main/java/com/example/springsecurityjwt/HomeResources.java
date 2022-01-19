bcf64ad8025c8e464e840452131e1d5ce6b67816
NJBodam <bodambest@yahoo.com>
2022-01-19 15:23:34 +0100

    changed /hello API to a get method

--- Desktop/springsecurityjwt/src/main/java/com/example/springsecurityjwt/HomeResources.java
+++ Desktop/springsecurityjwt/src/main/java/com/example/springsecurityjwt/HomeResources.java
@@ -24,7 +24,7 @@ public class HomeResources {
     @Autowired
     private MyUserDetailsService userDetailsService;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.POST)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "Hello World";
     }
